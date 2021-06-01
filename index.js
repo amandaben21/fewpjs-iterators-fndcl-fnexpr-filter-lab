404b2458d99cdfdb422f0360ca11b5575d21f588
Amanda <amandaben21@aol.com>
2021-06-01 15:25:38 -0400

    commit tree 2e104245612e8264131d641984e9b2210b701b76

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+
+/* returns all drivers that match the passed in name
+   returns matching drivers if case does not match but letters do
+   returns an empty array if there is no match */
+function findMatching(drivers, string) {
+    return drivers.filter (diver => diver.toLowerCase() === string.toLowerCase())
+}
+
+
+/* returns a driver if beginning provided letters match
+   does not return drivers if only middle or ending letters match
+   does not return drivers if only middle or ending letters match*/
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(diver => diver.slice(0, string.length) === string)
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(driver => driver.name === string)
+  }
